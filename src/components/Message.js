d851143ca54ebc357bfa7119b2277cf52105d3c9
ToluwaDavid <mailtolulopedavid@gmail.com>
2022-11-15 15:59:40 +0100

    First Commit added

---
+++ src/components/Message.js
@@ -0,0 +1,16 @@
+import React, { Children } from 'react'
+import {Alert} from 'react-bootstrap'
+
+export const Message = ({variant, Children}) => {
+  return 
+   <Alert variant={variant}>
+        {Children}
+   </Alert>
+}
+
+   Message.defaultProps = {
+    variant: 'info'
+   }
+
+export default Message
+  
